ddac2469f3259b9a23c05108e716098e6a1b63d0
David Velarde <2020030241@upsin.edu.mx>
2023-05-15 20:48:19 -0700

    Initial commit

---
+++ app/src/main/java/com/example/practica01_92/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.practica01_92;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnPulsar;
+    private EditText txtNombre;
+    private TextView lblSaludar;
+    private Button btnCerrar;
+
+
+    private Button btnLimpiar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Relacionar los objetos
+        btnPulsar= (Button) findViewById(R.id.btnSaludar);
+        txtNombre= (EditText) findViewById(R.id.txtNombre);
+        lblSaludar=(TextView) findViewById(R.id.lblSaludo);
+        btnLimpiar=(Button) findViewById(R.id.btnLimpiar);
+        btnCerrar=(Button) findViewById(R.id.btnCerrar);
+
+        //Codificar el evento clic del Boton
+        btnPulsar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Validar
+                if(txtNombre.getText().toString().matches(""))
+                {
+                    Toast.makeText(MainActivity.this,"Falto Capturar Información",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String str= "Hola "+txtNombre.getText().toString()+" ¿Como estas?";
+                    lblSaludar.setText(str.toString());
+                    //Finish
+                    //Limpiar
+                    //Kotlin
+
+                }
+            }
+        });
+
+        btnLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Validamos el limpiar
+                txtNombre.setText("");
+                lblSaludar.setText("::");
+            }
+        });
+
+        btnCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
+
+    }
+}
